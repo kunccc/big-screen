e0fc78d8cd8750586024fb22d4f3a7fb50907cc9
Hee <kuncc@foxmail.com>
2021-05-12 19:36:59 +0800

    update：add Chart7

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,6 +7,7 @@ import {Chart3} from '../charts/Chart3';
 import {Chart4} from '../charts/Chart4';
 import {Chart5} from '../charts/Chart5';
 import {Chart6} from '../charts/Chart6';
+import {Chart7} from '../charts/Chart7';
 
 export const Home = () => {
   return (
@@ -26,6 +27,7 @@ export const Home = () => {
         <section className="section3">
           <Chart6/>
           <Chart3/>
+          <Chart7/>
         </section>
       </main>
     </div>
